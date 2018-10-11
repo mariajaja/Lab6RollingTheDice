b4ad817b33a6b183c960f0e034f778ec0d881fc0
Mariah Hall <hallmar94@gmail.com>
2018-10-11 14:51:13 -0400

    fixed loop for rolling again, created random number

--- src/DiceRollerApp.java
+++ src/DiceRollerApp.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -19,20 +20,34 @@ public class DiceRollerApp {
 	public static void main(String[] args) {
 		Scanner userContinueQuestion = new Scanner(System.in);
 		Scanner userDiceQuestion = new Scanner(System.in);
+		int userDiceDecision;
+//		String userStartAgain;
+		String userContinueResponse;
 
 		System.out.println("Welcome to the Grand Circus Casino!\n");
 
 		do {
-			// add scanner question about sides on the die
-			// implement method to generate random roll
-			// display results of the two di
-			// implement method for special messages
-			// add scanner question about continuing
+			Random rand = new Random(); // creates random number generator
 
-		} while (userContinue.equals('y'));
+			// allows user input about sides on the die
+			System.out.print("How many sides should each die have?");
+			userDiceDecision = userDiceQuestion.nextInt();
+
+			// implement method to generate random roll
+			int diceResultFirst = rand.nextInt(userDiceDecision) + 1;
+			int diceResultSecond = rand.nextInt(userDiceDecision) + 1;
+
+			// call method for calling out results of di and special rolls
+
+			// question about rolling again with same amount of sides
+			System.out.println("Roll again? (y/n)");
+			userContinueResponse = userContinueQuestion.nextLine();
+
+			// add a way to start completely over? maybe
+		} while (userContinueResponse.equals("y"));
 	}
 
 	// create method to generate random numbers for the rolls
 	// create method if user got craps, snake eyes and box cars
-	// google what those are
+	// google what those^ are
 }
