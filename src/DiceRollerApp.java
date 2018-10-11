454b8f2c1c61ad1fd27468107a609f146be849b7
Mariah Hall <hallmar94@gmail.com>
2018-10-11 12:57:00 -0400

    initial commit of lab 6

---
+++ src/DiceRollerApp.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author Mariah
+ *
+ */
+public class DiceRollerApp {
+
+	/**
+	 * Ask user how many sides they want their dice to have, prompt user to roll
+	 * dice and display the results of the rolls. After, user will be asked if they
+	 * want to play again.
+	 */
+
+	public static void main(String[] args) {
+		Scanner userContinueQuestion = new Scanner(System.in);
+		Scanner userDiceQuestion = new Scanner(System.in);
+
+		System.out.println("Welcome to the Grand Circus Casino!\n");
+
+		do {
+			// add scanner question about sides on the die
+			// implement method to generate random roll
+			// display results of the two di
+			// implement method for special messages
+			// add scanner question about continuing
+
+		} while (userContinue.equals('y'));
+	}
+
+	// create method to generate random numbers for the rolls
+	// create method if user got craps, snake eyes and box cars
+	// google what those are
+}
